54e819d7f246807f2f26edb9d4df5a3c60ee2024
Erika Tavera <dugarerika@gmail.com>
2020-09-15 22:08:15 +0300

    creo proyecto doble_binding

---
+++ 03_doble_bindig/js/main.js
@@ -0,0 +1,39 @@
+// Nodos del DOM
+
+const bBorrar = document.querySelector('#b_borrar')
+const iNombre = document.querySelector()
+
+// Asignacion de manejadores de eventos
+bMostar.addEventListener('click', onClickMostrar)
+
+
+
+// Funciones manejadoras de eventos
+
+function onClickMostrar(){
+    nombre = iNombre.value
+    oResult.value = nombre
+}
+
+/**
+ * Funcion manejadora del evento Borrar del boton b_borrar
+ * @author Erika Maria Tavera Misas
+ */
+function onClickBorrar(){
+    nombre = ""
+    iNombre.value = nombre
+    oResult.value = nombre
+}
+
+/**
+ * @param 
+ */
+function onInputNombre(){
+     nombre = iNombre.value
+     oResult.value = nombre
+}
+
+
+
+/** Comentario JSDOC */
+
