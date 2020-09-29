ce875469aefb23b37741d658d9da091e4d0a25f2
Erika Tavera <dugarerika@gmail.com>
2020-09-30 01:42:40 +0300

    optimizo el codigo en el archivo app.js

--- 05_sitio_web_footer/js/app.js
+++ 05_sitio_web_footer/js/app.js
@@ -1,9 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#b_acceder').addEventListener('click', () => {
-        
+function main () {
+    const btn = document.querySelector('#b_acceder')
+
+    if(btn){
+        btn.addEventListener('click', onClick)
+    }
+
+    function onClick () {
+
         const formLogin = document.querySelector('#f_login')
-        const inputs = [...formLogin.querySelectorAll('input')] //convierto a un array con puntitos delante y poniendo dentro de unas llaves
-        console.dir(inputs)
+        const inputs = [...formLogin.querySelectorAll('input')]
         
         try {
 
@@ -12,19 +17,16 @@ document.addEventListener('DOMContentLoaded', () => {
                     throw new Error(`Campo ${item[i].id} Invalido`)
                 }
             })
-        //     for (let i = 0; i < inputs.length; i++) {
-        //         if(!inputs[i].value){
-        //             throw new Error(`Campo ${inputs[i].id} Invalido`)
-        //         }   
-        //     }
+
         } catch (error) {
             console.log(error.message)
             formLogin.querySelector('p').innerHTML = error.message
             return
         }
 
-        // BOM 
-        window.location = 'usuario.html' // se puede dejar sin el windows
-    })
-})
+        window.location = 'usuario.html'
+    }
+}
 
+
+document.addEventListener('DOMContentLoaded', main)
