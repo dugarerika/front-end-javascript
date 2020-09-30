edd740a603df3f715c58521671ac9f4bf6106e42
Erika Tavera <dugarerika@gmail.com>
2020-09-30 16:18:02 +0300

    creo proyecto 06_sitio_web_header

---
+++ 06_sitio_web_header/js/app.js
@@ -0,0 +1,36 @@
+import {templFooter} from '../templates/footer.js'
+
+function main () {
+
+    const btn = document.querySelector('#b_acceder')
+
+    if(btn){
+        btn.addEventListener('click', onClick)
+    }
+
+    document.querySelector('footer').innerHTML = templFooter
+    
+    function onClick () {
+
+        const formLogin = document.querySelector('#f_login')
+        const inputs = [...formLogin.querySelectorAll('input')]
+
+        try {
+
+            inputs.forEach((item)=> {
+                if(!item.value){
+                    throw new Error(`Campo ${item[i].id} Invalido`)
+                }
+            })
+
+        } catch (error) {
+            console.log(error.message)
+            formLogin.querySelector('p').innerHTML = error.message
+            return
+        }
+
+        window.location = 'usuario.html'
+    }
+}
+
+document.addEventListener('DOMContentLoaded', main)
---
+++ 06_sitio_web_header/templates/footer.js
@@ -0,0 +1,5 @@
+export const templFooter = ` 
+<address>Erika Maria Tavera Misas</address>
+<p>dugarerika@gmail.com.co</p>
+<p>${(new Date()).toLocaleDateString()} </p>
+`
