1e34edba8ccf29f864d0c432e2b1e7ed6fa31173
Erika Tavera <dugarerika@gmail.com>
2020-10-01 21:04:58 +0300

    codigo para que el usuario sea no casesensitive

--- 07_sitio_web_registro/js/app.js
+++ 07_sitio_web_registro/js/app.js
@@ -2,7 +2,6 @@ import {templFooter} from '../templates/footer.js'
 import { templHeader } from '../templates/header.js'
 
 function main () {
-
     const storeUsers = 'usuarios'
     const btnLog = document.querySelector('#b_acceder')
     const btnReg = document.querySelector('#b_registrar')
@@ -23,30 +22,33 @@ function main () {
     document.querySelector('header').innerHTML = templHeader.render(page)
 
     function onClickLog () {
-    
         const formLogin = document.querySelector('#f_login')
-
         if (!validarForm(formLogin)) {
             return
         }
-
         const users = window.localStorage.getItem(storeUsers) ?
         JSON.parse(window.localStorage.getItem(storeUsers)) : []
         const inputs = [...formLogin.querySelectorAll('input')]
 
-        let index = users.find(item => item.name == inputs[0].value)
-        console.log(index)
-
-        // window.location = 'usuario.html'
+        let findUser = users.find(item => item.name.toUpperCase() == inputs[0].value.toUpperCase())
+        console.log(findUser)
+        if (!findUser){
+            console.log('Usuario no encontrado')
+        }
+        else if (findUser.pwd != inputs[1].value) {
+            console.log('Password incorrecta') //sacar mensaje al usuario por medio de mensajes
+        }
+        else {
+            console.log('Usario y password correctos')
+            window.location = 'usuario.html'
+        }
     }
 
     function onClickReg () {
-    
         const formReg = document.querySelector('#f_registro')  
         if (!validarForm(formReg)) {
             return
         }
-
         const inputs = [...formReg.querySelectorAll('input')]
         const usuario = {
             mail : inputs[0].value,
@@ -56,14 +58,15 @@ function main () {
         const users = window.localStorage.getItem(storeUsers) ?
         JSON.parse(window.localStorage.getItem(storeUsers)) : []
         users.push(usuario)
+        
         window.localStorage.setItem(storeUsers, JSON.stringify(users))
-
+        inputs.forEach(item => item.value = '')
+        // location.href= 'index.html'
+        // window.location = 'index.html'
     }
 
     function validarForm(form) {
-
         const inputs = [...form.querySelectorAll('input')]
-
         try {
             inputs.forEach((item)=> {
                 if(!item.value){
