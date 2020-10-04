e8c51ccf6afa391f03fd871ec4fae01d99370b05
Erika Tavera <dugarerika@gmail.com>
2020-10-04 23:12:49 +0300

    realizo ajuster en los proyectos 11 y 12

--- 12_Formulario_Pro/js/app.js
+++ 12_Formulario_Pro/js/app.js
@@ -1,25 +1,45 @@
 function main(){
-    const form = document.querySelector('#formUsers')
-    form.addEventListener('submit', sendData)
 
-    // completar el select
-    const aRegiones = ['Madrid','Cataluña','Euskadi']
+    // Nodos del DOM
+    const form = document.querySelector('#formUsers')
+    const aGenero = [...form.querySelectorAll('[name="genero"]')]
+    const nombre = form.querySelector('input#nombre').value
+    const condiciones = form.querySelector('input#condiciones')
+    const region = form.querySelector('select#regiones')
+
+    // Definicion de manejadores de eventos
+    form.addEventListener('submit', sendData)
+    nombre.addEventListener('focus', nombreManager)
+    nombre.addEventListener('blur', nombreManager)
+    
+    // Completar el select
+    const aRegiones = ['Madrid','Cataluña','Euskadi', 'Galicia', 'Andalucia']
     setSelect('regiones', aRegiones)
 
+    // Funciones manejadoras
     function sendData(ev){
         const data = {}
         ev.preventDefault()
-        const genero = [...form.querySelectorAll('[name="genero"]')]
-        console.log(genero)
+
         data.genero = genero.filter(item => item.checked)[0].value
-        data.nombre = form.querySelector('input#nombre').value
-        data.acepto_condiciones = form.querySelector('input#condiciones').checked
-        data.region = form.querySelector('select#regiones').value
+        data.nombre = nombre.value
+        data.acepto_condiciones = condiciones.checked
+        data.region = region.value
         console.log('Obteniendo Datos', data)
         // fetch()
         console.log('Enviando')
     }
 
+        function nombreManager(ev){
+            if (ev.type === 'focus'){
+                console.log('El control gano el foco')
+            }else if (ev.type === 'blur'){
+                console.log('El control perdio el foco')
+            }
+
+        }
+
+    // otras funciones
     function setSelect(id, data) {
         let html = '<option></option>`'
         data.forEach(item => html += `
